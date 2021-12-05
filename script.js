dfb5e8703e10e47232e87fd5cbd25b471582aff0
A DIONN <ashley.muldrow@yahoo.com>
2021-12-05 16:57:08 -0500

    css changes

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ $("#currentDay").append(currentDay);
 
 var currentTime = moment().format("LT");
 $("#currentTime").append(currentTime);
+
+// Save Button 
+$(document). ready(function () {
+    $(".saveBtn").on("click", function () {
+        var text =$(this).siblings(".description").val();
+        var time=$(this).parent().attr("id");
+
+    // save text
+    localStorage.setItem(time, text);
+
+})
+
